f4ddca25711e01fbba3f02aeb9a0338f8c6ac0fb
a.ivanov <a.ivanov@aivanov.local>
2017-01-30 22:20:01 +0300

    lesson #9 done

--- MySQLConnect.php
+++ MySQLConnect.php
@@ -1,12 +1,40 @@
 <?php
+require "MySQLResultSet.php";
 
-/**
- * Created by PhpStorm.
- * User: a.ivanov
- * Date: 30.01.17
- * Time: 21:24
- */
 class MySQLConnect
 {
+    private $connection;
+    static $instances = 0;
+
+    public function __construct($hostname, $username, $password)
+    {
+        if (self::$instances == 0) {
+            $this->connection = mysql_connect($hostname, $username, $password) or
+            die(mysql_error() . 'Ошибка #' . mysql_errno());
+            self::$instances = 1;
+        } else {
+            die('Закройте предыдущее соединение');
+        }
+
+    }
+
+    public function __destruct()
+    {
+        // TODO: Implement __destruct() method.
+    }
+
+    public function close()
+    {
+        self::$instances = 0;
+        if (isset($this->connection)) {
+            mysql_close($this->connection);
+            unset($this->connection);
+        }
+    }
+
+    public function createResultSet($strSQL, $databasename) {
+        $rs = new MySQLResultSet($strSQL, $databasename, $this->connection);
+        return $rs;
+    }
 
 }
--- index9.php
+++ index9.php
@@ -1,7 +1,49 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: a.ivanov
- * Date: 30.01.17
- * Time: 21:45
- */
+require "MySQLConnect.php";
+require "PageNavigator.php";
+
+define('PERPAGE', 3);
+define('OFFSET', 'offset');
+$offset = $_GET[OFFSET];
+
+if (!isset($offset)) {
+    $recordoffset = 0;
+} else {
+    $recordoffset = $offset * PERPAGE;
+}
+
+$category = isset($_GET['category']) ? $_GET['category'] : 'LIT';
+
+$strSQL = 'SELECT author, title ' .
+            'FROM tblbooks ' .
+            "WHERE sold = 0 AND cat = '$category' " .
+            "ORDER BY author LIMIT $recordoffset," . PERPAGE;
+
+$con = new MySQLConnect('localhost', 'root', '');
+$rs = $con->createResultSet($strSQL, 'php5book');
+
+?>
+
+<div style="text-align: center">
+    <?php
+    while ($row = $rs->getRow()) {
+        echo $row[0] . ' - ' . $row[1];
+        echo "<br />\n";
+    }
+    ?>
+    <br>
+</div>
+
+<?php
+$pagename = basename($_SERVER['PHP_SELF']);
+$totalrecords = $rs->getUnlimitedNumberRows();
+$numpages = ceil($totalrecords / PERPAGE);
+
+$otherparameters = '&amp;category=' . $category;
+
+if ($numpages > 1) {
+    $nav = new PageNavigator($pagename, $totalrecords, PERPAGE, $recordoffset, 4, $otherparameters);
+    echo $nav->getNavigator();
+}
+
+?>
